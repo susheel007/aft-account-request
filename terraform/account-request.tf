e553e70e27eb259bae88f04642a75da5c72faaf3
susheel007 <chennoju.susheel@gmail.com>
2025-09-16 00:49:45 -0500

    add account 3

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -39,3 +39,45 @@ module "sandbox_account_01" {
 
   account_customizations_name = "sandbox-customizations"
 }
+
+
+#Account3
+
+module "sandbox_account_03" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "writetomycompany+user3@gmail.com"
+    AccountName  = "sandbox-account-03"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Sandbox"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "writetomycompany+user3@gmail.com"
+    SSOUserFirstName = "Susheel"
+    SSOUserLastName  = "Cchennoju"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "writetomycompany+user3@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Susheel Doe"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "sandbox-customizations"
+}
